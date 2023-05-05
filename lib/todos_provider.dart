1becf51cfee0bd1d69c6761570e841fd5a840d5e
Yann Attakoui <yannatkk@gmail.com>
2023-05-05 17:25:04 +0200

    refactor: get todosProvider as a list

--- lib/todos_provider.dart
+++ lib/todos_provider.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/todo_model.dart';
 
-final todosProvider = StateProvider<Todo>((ref) {
-  return Todo(
-    id: 0,
-    title: '',
-    isDone: false,
-  );
+final todosProvider = StateProvider<List<Todo>>((ref) {
+  return [
+    Todo(
+      id: 0,
+      title: 'Buy milk',
+      isDone: false,
+    ),
+    Todo(
+      id: 1,
+      title: 'Buy eggs',
+      isDone: false,
+    ),
+    Todo(
+      id: 2,
+      title: 'Buy bread',
+      isDone: false,
+    ),
+  ];
 });
