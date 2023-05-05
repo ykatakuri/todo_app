46a11caaf1e71e82835b8cc6bbab29c397735747
Yann Attakoui <yannatkk@gmail.com>
2023-05-05 16:45:36 +0200

    refactor: wrap the app as provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
