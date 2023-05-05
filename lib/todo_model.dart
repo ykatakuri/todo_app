a9728fbec60ba641d6659c33d2bb7edb040752f3
Yann Attakoui <yannatkk@gmail.com>
2023-05-05 12:31:30 +0200

    refactor: modify id and title type AND add fromJson and toJson to parse data

--- lib/todo_model.dart
+++ lib/todo_model.dart
@@ -1,11 +1,23 @@
 class Todo {
-  final String? id;
-  final String title;
-  final bool? isDone;
+  int? id;
+  String title;
+  bool? isDone;
 
   Todo({
     this.id,
     required this.title,
     this.isDone,
   });
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+        id: json['id'] as int?,
+        title: json['title'] as String,
+        isDone: json['isDone'] as bool?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'isDone': isDone,
+      };
 }
