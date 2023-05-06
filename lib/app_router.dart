2fbe434960d099881a947da1b2d1ce0723fc0123
Yann Attakoui <yannatkk@gmail.com>
2023-05-06 23:36:02 +0200

    refactor: remove todos_screen

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/home_screen.dart';
-import 'package:todo_app/todos_screen.dart';
 
 enum AppRoute {
   home,
@@ -23,23 +22,7 @@ final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) =>
-          const HomeScreen(title: 'ToDo App Home Screen'),
-      routes: [
-        GoRoute(
-          path: AppRoute.home.path,
-          name: AppRoute.home.name,
-          builder: (context, state) =>
-              const HomeScreen(title: 'ToDo App Home Screen'),
-          routes: [
-            GoRoute(
-              path: AppRoute.todos.path,
-              name: AppRoute.todos.name,
-              builder: (context, state) => const TodosScreen(),
-            ),
-          ],
-        ),
-      ],
+      builder: (context, state) => const HomeScreen(title: 'Tvskrz'),
     ),
   ],
   debugLogDiagnostics: true,
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,30 +1,130 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:todo_app/app_router.dart';
+import 'dart:math';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required this.title});
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/todo_model.dart';
+import 'package:todo_app/todos_provider.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({
+    required this.title,
+    super.key,
+  });
 
   final String title;
 
   @override
+  ConsumerState<HomeScreen> createState() => _TodosScreenState();
+}
+
+class _TodosScreenState extends ConsumerState<HomeScreen> {
+  final todoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final todos = ref.watch(todosProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: Column(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
+        child: ListView(
           children: [
-            ListTile(
-              title: const Text('Add a todo'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () => context.goNamed(AppRoute.todos.path),
+            TextFormField(
+              controller: todoController,
+              decoration: const InputDecoration(
+                labelText: 'What needs to be done?',
+                border: OutlineInputBorder(),
+              ),
+              onFieldSubmitted: (value) {
+                if (value.isNotEmpty) {
+                  ref.read(todosProvider.notifier).update((state) => [
+                        ...state,
+                        Todo(
+                          id: Random().nextInt(100),
+                          title: value,
+                          isDone: false,
+                        ),
+                      ]);
+
+                  todoController.clear();
+                }
+              },
             ),
-            const Divider(
-              color: Colors.grey,
-            ),
+            const SizedBox(height: 50),
+            if (todos.isNotEmpty) ...[
+              Text(
+                'TODOs',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+
+                  if (todo.isDone!) {
+                    return ListTile(
+                      title: Text(
+                        todo.title,
+                        style: const TextStyle(
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      leading: Checkbox(
+                        value: todo.isDone,
+                        onChanged: (value) {
+                          ref.read(todosProvider.notifier).state = [
+                            for (final todo in todos)
+                              if (todo.id == todos[index].id)
+                                Todo(
+                                  id: todo.id,
+                                  title: todo.title,
+                                  isDone: value,
+                                )
+                              else
+                                todo
+                          ];
+                        },
+                        activeColor: Colors.grey,
+                      ),
+                      textColor: Colors.grey,
+                    );
+                  } else {
+                    return ListTile(
+                      title: Text(todo.title),
+                      leading: Checkbox(
+                        value: todo.isDone,
+                        onChanged: (value) {
+                          ref.read(todosProvider.notifier).state = [
+                            for (final todo in todos)
+                              if (todo.id == todos[index].id)
+                                Todo(
+                                  id: todo.id,
+                                  title: todo.title,
+                                  isDone: value,
+                                )
+                              else
+                                todo
+                          ];
+                        },
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
           ],
         ),
       ),
