cf745be2020d0b4c6d98cf6b34cce7a790124c70
Yann Attakoui <yannatkk@gmail.com>
2023-05-08 17:07:15 +0200

    refactor: update service provider to read and update todos state

--- lib/todos_provider.dart
+++ lib/todos_provider.dart
@@ -2,27 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/todo_model.dart';
 import 'package:todo_app/todo_service.dart';
 
-final todosProvider = StateProvider<List<Todo>>((ref) {
-  return [
-    Todo(
-      id: 0,
-      title: 'Buy milk',
-      isDone: false,
-    ),
-    Todo(
-      id: 1,
-      title: 'Buy eggs',
-      isDone: false,
-    ),
-    Todo(
-      id: 2,
-      title: 'Buy bread',
-      isDone: false,
-    ),
-  ];
+// provider to write todo to firestore
+final todoServiceProvider = Provider.autoDispose<TodoService>((ref) {
+  return TodoService();
 });
 
-// provider to write todo to firestore
-final todoServiceProvider = Provider<TodoService>((ref) {
-  return TodoService();
+// provider to read todo from firestore
+final todosProvider = StreamProvider.autoDispose<List<Todo>>((ref) {
+  final todoService = ref.watch(todoServiceProvider);
+  return todoService.readTodos();
 });
