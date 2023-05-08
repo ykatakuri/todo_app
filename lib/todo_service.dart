e2df0e82ebf46b646c8546019c84a0e10f682852
Yann Attakoui <yannatkk@gmail.com>
2023-05-08 17:06:06 +0200

    refactor: create, read and update methods

--- lib/todo_service.dart
+++ lib/todo_service.dart
@@ -2,11 +2,46 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo_app/todo_model.dart';
 
 class TodoService {
-  Future<DocumentReference> addTodo(Todo todo) {
-    return FirebaseFirestore.instance.collection('todos').add(<String, dynamic>{
+  // Get an instance of FirebaseFirestore
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<void> addTodo(Todo todo) {
+    return firestore.collection('todos').add(<String, dynamic>{
+      'id': '',
       'title': todo.title,
       'isDone': todo.isDone,
       'timestamp': DateTime.now().millisecondsSinceEpoch,
+    }).then((value) => firestore.collection('todos').doc(value.id).update({
+          'id': value.id,
+        }));
+  }
+
+  // read todo from firestore
+  Stream<List<Todo>> readTodos() {
+    return firestore
+        .collection('todos')
+        .orderBy('timestamp', descending: true)
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Todo(
+          id: doc['id'] as String,
+          title: doc['title'] as String,
+          isDone: doc['isDone'] as bool,
+        );
+      }).toList();
+    });
+  }
+
+  // update todo in firestore
+  Future<void> updateTodo({
+    required String id,
+    required String title,
+    required bool isDone,
+  }) {
+    return firestore.collection('todos').doc(id).update(<String, dynamic>{
+      'title': title,
+      'isDone': isDone,
     });
   }
 }
